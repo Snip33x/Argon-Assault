2d6db93a0fcf9ec9c3f29517fa417741904f5a04
Snip33x <r.kulawiak95@gmail.com>
2021-04-13 22:24:45 +0200

    Timeline

---
+++ Assets/Scripts/PlayerControlls.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+// new INsys using UnityEngine.InputSystem;
+
+public class PlayerControlls : MonoBehaviour
+{
+    //new INsys [SerializeField] InputAction movement;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+
+    // to jest potrzebne jak używamy nowego input systemu
+    //private void OnEnable()
+    //{
+    //    movement.Enable();
+    //}
+
+    //private void OnDisable()
+    //{
+    //    movement.Disable();
+    //}
+
+    // Update is called once per frame
+    void Update()
+    {
+        //new INsys float horizontalThrow = movement.ReadValue<Vector2>().x;
+        //new INsys float verticalThrow = movement.ReadValue<Vector2>().y;
+
+
+        float xThrow = Input.GetAxis("Horizontal");
+        Debug.Log("horizontal " + xThrow);
+
+        float yThrow = Input.GetAxis("Vertical");
+        Debug.Log("vertical " + yThrow);
+    }
+}
