cd066e7c0544f5211521d419e57a83ebacaf4cbe
DESKTOP-IQQMKC0\Snip33x <r.kulawiak95@gmail.com>
2021-05-24 23:52:04 +0200

    Before Particle system

--- Assets/Scripts/PlayerControlls.cs
+++ Assets/Scripts/PlayerControlls.cs
@@ -10,6 +10,14 @@ public class PlayerControlls : MonoBehaviour
     [SerializeField] float xRange = 15f;
     [SerializeField] float yRange = 10f;
 
+    [SerializeField] float positionPitchFactor = -2f;
+    [SerializeField] float controlPitchFactor = -10f;
+
+    [SerializeField] float positionYawFactor = 2f;
+    [SerializeField] float controlRollFactor = -20f;
+
+    float xThrow , yThrow;
+
     //new INsys [SerializeField] InputAction movement;
 
     // Start is called before the first frame update
@@ -35,22 +43,43 @@ public class PlayerControlls : MonoBehaviour
     {
         //new INsys float horizontalThrow = movement.ReadValue<Vector2>().x;
         //new INsys float verticalThrow = movement.ReadValue<Vector2>().y;
+
+
+        ProcessTranslation();
+        ProcessRotation();
+
+    }
+
+    void ProcessRotation()
+    {
+        float pitchDueToPosition = transform.localPosition.y * positionPitchFactor;
+        float pitchDueToControlThrow = yThrow * controlPitchFactor;
+
+        float yawDueToPosition = transform.localPosition.x * positionYawFactor;
+
         
 
-        float xThrow = Input.GetAxis("Horizontal");
-        float yThrow = Input.GetAxis("Vertical");
+        float pitch =  pitchDueToPosition + pitchDueToControlThrow;
+        float yaw = yawDueToPosition;
+        float roll = xThrow * controlRollFactor ;
+
+
+        transform.localRotation = Quaternion.Euler(pitch, yaw, roll);
+    }
+
+    private void ProcessTranslation()
+    {
+        xThrow = Input.GetAxis("Horizontal");
+        yThrow = Input.GetAxis("Vertical");
 
         float xOffset = xThrow * Time.deltaTime * controlSpeed;
         float rawXPos = transform.localPosition.x + xOffset;
-        float clampedXPos = Mathf.Clamp(rawXPos, -xRange, xRange);
-        
+        float clampedXPos = Mathf.Clamp(rawXPos, -xRange, xRange); // robienie zakresów w jakich będzie się poruszać statek 
+
         float yOffset = yThrow * Time.deltaTime * controlSpeed;
         float rawYPos = transform.localPosition.y + yOffset;
-        float clampedYPos = Mathf.Clamp(rawYPos, -yRange + 3, yRange);  // statek zjeżdzał poniżej dolnej krawędzi ekranu , Rick przestawił kamerę do dołu - ja dodałem tu trójeczkę - oba sposoby działają
+        float clampedYPos = Mathf.Clamp(rawYPos, -yRange + 7, yRange);  // statek zjeżdzał poniżej dolnej krawędzi ekranu , Rick przestawił kamerę do dołu - ja dodałem tu trójeczkę - oba sposoby działają
 
         transform.localPosition = new Vector3(clampedXPos, clampedYPos, transform.localPosition.z);
-
-
-
     }
 }
