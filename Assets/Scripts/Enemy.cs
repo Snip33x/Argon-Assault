444086deaba7c86adb309d4fd7d4a281e850f198
Rafał Kulawiak <62555963+Snip33x@users.noreply.github.com>
2021-06-06 22:30:30 +0200

    Add Enenmy HitPoints and hit VFX

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,12 +1,14 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    [SerializeField] GameObject hitVFX;
     [SerializeField] GameObject deathVFX;
     [SerializeField] Transform parent;
-    [SerializeField] int scorePerHit = 100;
+    [SerializeField] int scorePerHit = 15;
+    [SerializeField] int hitPoints = 2;
 
     ScoreBoard scoreBoard;
 
@@ -20,19 +22,26 @@ public class Enemy : MonoBehaviour
     private void OnParticleCollision(GameObject other)
     {
         ProcessHit();
-        KillEnemy();
+        if (hitPoints < 1)  // mniejsze niż 1 jest lepsze niż == 0 bo jest odporniejsze na błędy
+        {
+            KillEnemy();
+        }
     }
 
     private void ProcessHit()
     {
+        GameObject vfx = Instantiate(hitVFX, transform.position, Quaternion.identity); //quaternion.identity to jest poprostu aktualna rotacja bez zmian tak jak pozycja
+        vfx.transform.parent = parent;
+        //Invoke("hitVFX", 1f);
         scoreBoard.IncreaseScore(scorePerHit);
+        hitPoints--;
     }
 
     private void KillEnemy()
     {
         GameObject vfx = Instantiate(deathVFX, transform.position, Quaternion.identity); //quaternion.identity to jest poprostu aktualna rotacja bez zmian tak jak pozycja
         vfx.transform.parent = parent;
-        Invoke("DestroyVFX", 1f);
+        //Invoke("DestroyVFX", 1f);
         Destroy(this.gameObject);
     }
 }
