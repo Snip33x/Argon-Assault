604a8454c3ff5f418bbc14835899bc3d07463a7b
Rafał Kulawiak <62555963+Snip33x@users.noreply.github.com>
2021-06-06 18:38:46 +0200

    Simple UI

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -6,11 +6,31 @@ public class Enemy : MonoBehaviour
 {
     [SerializeField] GameObject deathVFX;
     [SerializeField] Transform parent;
+    [SerializeField] int scorePerHit = 100;
+
+    ScoreBoard scoreBoard;
+
+    void Start()
+    {
+        scoreBoard = FindObjectOfType<ScoreBoard>(); // we have only one object of type score in hierarchy  bad to use in update()    
+        
+    }
 
 
     private void OnParticleCollision(GameObject other)
     {
-        GameObject vfx =  Instantiate(deathVFX, transform.position, Quaternion.identity); //quaternion.identity to jest poprostu aktualna rotacja bez zmian tak jak pozycja
+        ProcessHit();
+        KillEnemy();
+    }
+
+    private void ProcessHit()
+    {
+        scoreBoard.IncreaseScore(scorePerHit);
+    }
+
+    private void KillEnemy()
+    {
+        GameObject vfx = Instantiate(deathVFX, transform.position, Quaternion.identity); //quaternion.identity to jest poprostu aktualna rotacja bez zmian tak jak pozycja
         vfx.transform.parent = parent;
         Invoke("DestroyVFX", 1f);
         Destroy(this.gameObject);
--- Assets/Scripts/SelfDestruct.cs
+++ Assets/Scripts/SelfDestruct.cs
@@ -9,7 +9,7 @@ public class SelfDestruct : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Destroy(gameObject, 3f);
+        Destroy(gameObject, timeTillDestroy);
     }
 
 
