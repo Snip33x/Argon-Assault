68e770095086b9f9ccd34db7cf381610e7663087
Rafał Kulawiak <62555963+Snip33x@users.noreply.github.com>
2021-06-09 22:46:04 +0200

    FindWithTag() how it works, and starting point of Enemies waves creation

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -6,16 +6,18 @@ public class Enemy : MonoBehaviour
 {
     [SerializeField] GameObject hitVFX;
     [SerializeField] GameObject deathVFX;
-    [SerializeField] Transform parent;
     [SerializeField] int scorePerHit = 15;
     [SerializeField] int hitPoints = 2;
 
     ScoreBoard scoreBoard;
+    GameObject parentGameObject;
 
     void Start()
     {
         scoreBoard = FindObjectOfType<ScoreBoard>(); // we have only one object of type score in hierarchy  bad to use in update()    
         AddRigidbody();
+        parentGameObject = GameObject.FindWithTag("Spawn");  //wcześniej mieliśmy serializedField z parentGameObject do którego z poziomu Unity wrzucaliśmy obiekt SpawnAtRuntime, i dzięki temu tam wpadały klony, teraz znajdujemy poprostu to po tagu
+        
     }
 
     private void AddRigidbody()
@@ -36,7 +38,7 @@ public class Enemy : MonoBehaviour
     private void ProcessHit()
     {
         GameObject vfx = Instantiate(hitVFX, transform.position, Quaternion.identity); //quaternion.identity to jest poprostu aktualna rotacja bez zmian tak jak pozycja
-        vfx.transform.parent = parent;
+        vfx.transform.parent = parentGameObject.transform;
         //Invoke("hitVFX", 1f);
         scoreBoard.IncreaseScore(scorePerHit);
         hitPoints--;
@@ -45,7 +47,7 @@ public class Enemy : MonoBehaviour
     private void KillEnemy()
     {
         GameObject vfx = Instantiate(deathVFX, transform.position, Quaternion.identity); //quaternion.identity to jest poprostu aktualna rotacja bez zmian tak jak pozycja
-        vfx.transform.parent = parent;
+        vfx.transform.parent = parentGameObject.transform;
         //Invoke("DestroyVFX", 1f);
         Destroy(this.gameObject);
     }
